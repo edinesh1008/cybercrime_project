bdd08d8022dce74cbc6f34ad600ffeb5b90dbba7
DINESH ELUMALAI <edinesh108@gmail.com>
2026-02-04 23:41:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
 
-# Load model and encoders
+# Load files
 model = joblib.load("cybercrime_model.pkl")
 city_encoder = joblib.load("city_encoder.pkl")
 crime_encoder = joblib.load("crime_encoder.pkl")
@@ -9,18 +9,18 @@ location_encoder = joblib.load("location_encoder.pkl")
 
 st.title("Cybercrime Prediction System")
 
-# Dropdown inputs
-city = st.selectbox("Select City", city_encoder.classes_)
-crime = st.selectbox("Select Crime Type", crime_encoder.classes_)
+# Show real names
+city = st.selectbox("Select City", list(city_encoder.classes_))
+crime = st.selectbox("Select Crime Type", list(crime_encoder.classes_))
 amount = st.number_input("Enter Fraud Amount")
 
 if st.button("Predict"):
 
-    city_encoded = city_encoder.transform([city])
-    crime_encoded = crime_encoder.transform([crime])
+    city_encoded = city_encoder.transform([city])[0]
+    crime_encoded = crime_encoder.transform([crime])[0]
 
-    prediction = model.predict([[city_encoded[0], crime_encoded[0], amount]])
+    prediction = model.predict([[city_encoded, crime_encoded, amount]])
 
-    location = location_encoder.inverse_transform(prediction)
+    location_name = location_encoder.inverse_transform(prediction)[0]
 
-    st.success(f"Predicted Crime Location: {location[0]}")
+    st.success(f"Predicted Crime Location: {location_name}")
