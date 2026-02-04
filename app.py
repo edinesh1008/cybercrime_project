7c3ef093b124ef71295845facad76fec80e7cb77
DINESH ELUMALAI <edinesh108@gmail.com>
2026-02-04 23:51:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -8,7 +8,17 @@ columns = ["City","Crime_Type","Time_of_Crime","Victim_Age_Group",
            "Transaction_Mode","Bank_Type","Day_of_Week","Location"]
 
 for col in columns:
-    encoders[col] = joblib.load(f"{col}_encoder.pkl")
+    encoders = {
+    "City": joblib.load("City_encoder.pkl"),
+    "Crime_Type": joblib.load("Crime_Type_encoder.pkl"),
+    "Time_of_Crime": joblib.load("Time_of_Crime_encoder.pkl"),
+    "Victim_Age_Group": joblib.load("Victim_Age_Group_encoder.pkl"),
+    "Transaction_Mode": joblib.load("Transaction_Mode_encoder.pkl"),
+    "Bank_Type": joblib.load("Bank_Type_encoder.pkl"),
+    "Day_of_Week": joblib.load("Day_of_Week_encoder.pkl"),
+    "Location": joblib.load("Location_encoder.pkl")
+}
+
 
 st.title("Cybercrime Prediction System")
 
